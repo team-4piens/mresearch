1c87a783e79b6285547df9ed3069cb7fcde4ee57
cto-4piens <cto@4piens.com>
2020-10-15 16:04:18 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withImages = require('next-images')
 module.exports = withImages({
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '' : '',
     env: {
-        BACKEND_URL: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
+        BACKEND_URL: process.env.NODE_ENV === 'production' ? '' : '',
     },
 })
