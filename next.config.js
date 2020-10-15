7d274b9c19cac79966e41d8be63d94b9ad77eecd
cto-4piens <cto@4piens.com>
2020-10-15 10:24:03 +0900

    prefix url typo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     assetPrefix: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/mresearch' : '/',
+    basePath: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
     env: {
-        BACKEND_URL: process.env.NODE_ENV === 'production' ? '/mresearch' : '/',
+        BACKEND_URL: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
     }
 }
