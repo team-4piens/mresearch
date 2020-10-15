1aa1ca5c7c1b5aa4788292b2750222320bd8ec58
cto-4piens <cto@4piens.com>
2020-10-15 10:06:23 +0900

    add base prefix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/mresearch' : ''
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
 }
