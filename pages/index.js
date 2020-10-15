dbb735878b6a7f4295cf7700516e8353224f4217
cto-4piens <cto@4piens.com>
2020-10-15 10:23:15 +0900

    company link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,7 +13,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to a <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <Link href="/company"/>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
