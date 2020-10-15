d9c6df4d804a46ba35e34fd44db8dd2de0f1b061
cto-4piens <cto@4piens.com>
2020-10-15 10:08:49 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     assetPrefix: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/mresearch' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/mresearch' : '/mresearch',
 }
