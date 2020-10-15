c93014f5fd3578da39e8845057ca7430df55266a
cto-4piens <cto@4piens.com>
2020-10-15 10:06:43 +0900

    deploy to gh-pages

--- _next/static/3aa70upyzjme_0UTRLSqn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F9a89afcbe95084ea8b90.css","static\u002Fchunks\u002Fpages\u002Findex-f7a9c5a22c88a31ca0d0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
